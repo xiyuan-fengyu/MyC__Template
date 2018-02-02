e31ba587c206a3ee16a5404bc09de95c4f5d15f3
xiyuan_fengyu <xiyuan_fengyu@163.com>
2018-02-02 14:09:26 +0800

    增加 boost 库， json, logger

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,23 @@
+//
+// Created by xiyuan_fengyu on 2018/2/2.
+//
+
 #include <iostream>
+#include <functional>
+#include <vector>
+#include <thread>
+#include <boost/format.hpp>
+#include <boost/thread/thread_pool.hpp>
+#include <boost/lexical_cast.hpp>
+#include <boost/signals2/signal.hpp>
+#include <json.hpp>
+#include <logger.hpp>
+
+using namespace std;
+using namespace std::placeholders;
+using namespace nlohmann;
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+
     return 0;
 }
